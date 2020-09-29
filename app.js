1170ee240344257f1e0daffb55f67537966bd60d
Server <server@server.com>
2020-09-28 21:21:02 -0400

    environment variable

--- app.js
+++ app.js
@@ -5,15 +5,7 @@ var express                 = require("express"),
     methodOverride          = require("method-override"),
     flash                   = require("connect-flash");
 
-// mongoose.connect('mongodb://localhost:27017/superposition', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false
-// })
-// .then(() => console.log('Connected to DB!'))
-// .catch(error => console.log(error.message));
-
-mongoose.connect('mongodb+srv://adibahussain19:SPcolumbia2020@spcluster.zkxph.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(process.env.DATABASEURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
